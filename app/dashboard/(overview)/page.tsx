d8799627f8da6642cc93d449d2d62264409e0cfd
Hassan Raza <mrismart70@gmail.com>
2024-09-17 02:05:24 +0500

    Chapter 9 - Done Streaming Latest Invoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,16 +3,15 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import {
-  fetchLatestInvoices,
-  fetchCardData,
+  fetchCardData
 } from "@/app/lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
 
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
 
   const {
     totalPendingInvoices,
@@ -39,8 +38,11 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
       <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
-        </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+      </Suspense>
+      <Suspense fallback={<LatestInvoicesSkeleton/>}>
+      <LatestInvoices />
+        </Suspense>  
+        
       </div>
     </main>
   );
