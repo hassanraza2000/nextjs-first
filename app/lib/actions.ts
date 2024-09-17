3f0abc0da6786fc5019a1aa4dfe3b9afa5111c9d
Hassan Raza <mrismart70@gmail.com>
2024-09-18 01:44:30 +0500

    Chapter 12 - Done Revalidating and Redirecting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from "zod";
 import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -23,7 +25,10 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split("T")[0];
 
   await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
